a4cfaaf3c4f9c744c7137de023055c209ab01fe8
Sune Carelse <sune.carelse@takealot.com>
2021-03-09 22:24:49 +0200

    class based component

---
+++ src/AppClass.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter app</h1>
+        <p>The count is: {this.state.count}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
+  }
+}
