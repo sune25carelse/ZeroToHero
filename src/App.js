193f9de89df606efe97306b1a1b4c47d8b3df5f6
Sune Carelse <sune.carelse@takealot.com>
2021-03-09 21:36:40 +0200

    jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "sonny";
+  const isMale = true;
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {user}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      {getGreeting("Luca")}
+      <h2>You are a {isMale ? "Male" : "female"}</h2>
     </div>
   );
 }
