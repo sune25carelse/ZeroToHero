197a4656649e5c6cdac8f9933b7926bf32c8fa75
Sune Carelse <sune.carelse@takealot.com>
2021-03-11 22:10:29 +0200

    todo input and todoList with descriptns etc

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import React from "react";
-import { useRef } from "react";
-import { useState } from "react";
 import "./App.css";
-import Product from "./Product";
-import useRandomJoke from "./useRandomJoke";
+import Input from "./Input";
+import TodoItem from "./TodoItem";
+// import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 function App() {
-  const firstNameRef = useRef(null);
-  const lastNameRef = useRef(null);
-
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  const joke = useRandomJoke(firstName, lastName);
-
-  const generateJoke = (e) => {
-    e.preventDefault();
-    setFirstName(firstNameRef.current.value);
-    setLastName(lastNameRef.current.value);
-  };
-
+  const todoList = useState();
   return (
     <div className="app">
-      <h1>Luca Todo</h1>
-      <Product name="Amazon Echo" description="Ai assistant" price={59.99} />
-      <Product
-        name="Huawei P40 Pro"
-        description="Best Smartphone"
-        price={129.99}
-      />
+      <div className="app__container">
+        <div className="app__todoContainer">
+          {todoList.map((item) => (
+            <TodoItem name={item.item} done={item.done} id={item.id} />
+          ))}
+        </div>
 
-      <Product name="Predator" description="Gaming Laptop" price={39.99} />
-
-      <h2>The Joke Generator</h2>
-      <h3>{joke}</h3>
-
-      <form>
-        <input placeholder="firstName" ref={firstNameRef} />
-        <input placeholder="lastName" ref={lastNameRef} />
-        <button onClick={generateJoke}>Generate Joke</button>
-      </form>
+        <Input />
+      </div>
     </div>
   );
 }
