b465bf5c8436383cf0318e9cdf2ba5879e16cd27
Sune Carelse <sune.carelse@takealot.com>
2021-03-16 20:51:24 +0200

    firestore db attached

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyByd0Y-R0i7aEXpuZekFKYv9JDYmLbwysc",
   authDomain: "todo-app-zth.firebaseapp.com",
   projectId: "todo-app-zth",
@@ -9,7 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "679160192451",
   appId: "1:679160192451:web:a4e729c3e8429f60e2b176",
   measurementId: "G-Y4ZB219BPY",
-};
+});
 
 const db = firebaseApp.firestore();
 
