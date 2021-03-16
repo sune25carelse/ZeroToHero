cf806b6222fd045d2034830db07a3f8b7437a4fa
Sune Carelse <sune.carelse@takealot.com>
2021-03-16 20:37:05 +0200

    list avatar and item material-ui and style

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,21 @@
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@material-ui/core";
 import React from "react";
+import "./Todo.css";
 
 function Todo(props) {
   return (
-    <div>
-      <li>{props.text}</li>
+    <div className="todo">
+      <List>
+        <ListItem>
+          <ListItemAvatar></ListItemAvatar>
+          <ListItemText primary={props.text} secondary="Dummy-time⏰⏰" />
+        </ListItem>
+      </List>
     </div>
   );
 }
