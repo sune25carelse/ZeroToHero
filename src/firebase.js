7e6e47d8dae5d120c5f544e2f26f51232c81f6c5
Sune Carelse <sune.carelse@takealot.com>
2021-03-16 20:46:52 +0200

    npm firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyByd0Y-R0i7aEXpuZekFKYv9JDYmLbwysc",
+  authDomain: "todo-app-zth.firebaseapp.com",
+  projectId: "todo-app-zth",
+  storageBucket: "todo-app-zth.appspot.com",
+  messagingSenderId: "679160192451",
+  appId: "1:679160192451:web:a4e729c3e8429f60e2b176",
+  measurementId: "G-Y4ZB219BPY",
+};
+
+const db = firebaseApp.firestore();
+
+export default db;
